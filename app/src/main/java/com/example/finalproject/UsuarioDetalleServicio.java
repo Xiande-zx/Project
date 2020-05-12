1fb8eb9f9c6712111a987f9d8fd3cb1257b77f23
Xiande-zx <xqiu18@jaumebalmes.net>
2020-05-12 20:15:34 +0200

    2

---
+++ app/src/main/java/com/example/finalproject/UsuarioDetalleServicio.java
@@ -0,0 +1,14 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsuarioDetalleServicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario_detalle_servicio);
+    }
+}
